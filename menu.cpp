807e97cfdbd87bd97075860eab64c19941fedf88
Lucio Tamborini <59577462+luchimbo@users.noreply.github.com>
2020-08-14 10:56:40 -0300

    Add files via upload

--- menu.cpp
+++ menu.cpp
@@ -19,6 +19,7 @@ int opcion;
 string color;
 bool tinta;
 int cantidad;
+string tinta_texto = "Hola";
 
 do{
 	cout << "Modificar 1-Color 2-Tinta 3- Nro de Lapiceras 4-Salir" << endl;
@@ -30,19 +31,27 @@ do{
 			cin >> color;
 			break;
 		case 2:
-			cout << "Tiene tinta? Si-true No-false" << endl;
+			cout << "Tiene tinta? Si-1 No-0" << endl;
 			cin >> tinta;
 			break;
 		case 3:
 			cout << "Que numero de lapiceras tiene?" << endl;
 			cin >> cantidad;
 			break;
-	} while ( opcion != 4);
+	} 
+} while ( opcion != 4);
+
+if (tinta==true){
+	tinta_texto = "Tiene tinta";
 }
+	else{
+	tinta_texto = "No tiene tinta";
+	}
 
-	lapicera_t lapicera_1= {color, tinta, cantidad};
 
-	cout << lapicera_1.color << ' ' << lapicera_1.tinta << ' ' << lapicera_1.cantidad << ' ' << endl;
+lapicera_t lapicera_1 = {color, tinta, cantidad};
 
-	return 0;
+cout << lapicera_1.color << ' ' << lapicera_1.tinta << ' ' << tinta_texto << ' ' << lapicera_1.cantidad << ' ' << endl;
+
+return 0;
 }
